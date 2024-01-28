030f868067f1fdb19aa6ed315ee2df5658e5ed22
Oliver Treichel <oliver.treichel@gmx.de>
2024-01-28 16:13:04 +0100

    make hott-model a transitive dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module de.treichels.hott.serial {
-    requires kotlin.stdlib;
-    requires de.treichels.hott.util;
-    requires de.treichels.hott.model;
+    requires transitive de.treichels.hott.model;
     exports de.treichels.hott.serial;
     exports de.treichels.hott.serial.spi;
 }
